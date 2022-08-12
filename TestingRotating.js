cb294f600423e9d6225f18d6aa214552177a8401
bricenf <bricen2002@gmail.com>
2022-08-11 23:27:03 -0400

    aa

---
+++ TestingRotating.js
@@ -0,0 +1,9 @@
+// index.js
+window.onscroll = function () {
+    scrollRotate();
+};
+
+function scrollRotate() {
+    let image = document.getElementById("reload");
+    image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+}
